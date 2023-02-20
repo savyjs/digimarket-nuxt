3bfd18ea148b1a3b505765b94cdc7decdbdcb352
ehsan <savvyversa@gmail.com>
2023-02-20 17:27:40 +0330

    chore(logo): move to build folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,7 +49,7 @@ export default defineNuxtConfig({
         'assets/font-css/style.css'
     ],
     modules: [
-        // 'vite-plugin-vue-type-imports/nuxt',
+        'vite-plugin-vue-type-imports/nuxt',
         // Using package name
         // '@nuxt/content',
         // Relative to your project srcDir

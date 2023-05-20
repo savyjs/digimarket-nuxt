0a15536aa393c274c3b8358fdb1fd255aced534e
ehsan <savvyversa@gmail.com>
2023-05-20 16:59:49 +0330

    chore: add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,18 +5,5 @@ module.exports = {
     ],
     darkMode: ['class', '[data-mode="dark"]'],
     theme: {
-        extend: {
-            colors: {
-                transparent: 'transparent',
-                secondary: '#008eb2',
-                error: '#f44336',
-                success: '#1e7c1e',
-                alert: '#ff9800',
-                dark: '#212121',
-                primary: '#a41919',
-                accent: '#343232',
-                light: '#EEE',
-            },
-        },
     },
 };

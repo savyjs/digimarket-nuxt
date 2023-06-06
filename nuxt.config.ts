cc022c02590e75a08673843ee83e8a5dd0ba0acd
ehsan <savvyversa@gmail.com>
2023-06-06 15:58:01 +0330

    fix: rename module to layer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import {defineNuxtConfig} from "nuxt/config";
 
 export default defineNuxtConfig({
     extends: [
-        "digimarket-nuxt-module"
+        "digimarket-nuxt-layer"
     ],
     css: [
         'assets/font-css/fontiran.css',

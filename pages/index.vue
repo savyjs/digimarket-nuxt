1b40cfeeb5f9f77967d616669f64f08d3478ff9b
ehsan <savvyversa@gmail.com>
2023-06-06 16:05:11 +0330

    fix: rename fixturePrefix

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,30 @@
   <div class="m-5">
     <div class="m-5">
       <p class="">Nuxt pages:</p>
+      <p class="my-2">Active:</p>
       <div class="gap-3 flex">
-        Active:
-        <nuxt-link class="btn-success" :to="`${prefix}/user/login`">Login (mobile and password)</nuxt-link>
-        <nuxt-link class="btn-success-outlined" :to="`${prefix}/user/login-email-password`">Login (email and password)
+        <nuxt-link class="btn-success" :to="`${fixturePrefix}/user/login`">Login (mobile and password)</nuxt-link>
+        <nuxt-link class="btn-success-outlined" :to="`${fixturePrefix}/user/login-email-password`">Login (email and
+          password)
         </nuxt-link>
-        <nuxt-link class="btn-success-outlined" :to="`${prefix}/user/login-username-password`">Login (username and password)
+        <nuxt-link class="btn-success-outlined" :to="`${fixturePrefix}/user/login-username-password`">Login (username
+          and password)
         </nuxt-link>
-        <nuxt-link class="btn-success-outlined" :to="`${prefix}/user/login-oauth`">Login (OAuth)</nuxt-link>
-        <nuxt-link class="btn-success" :to="`${prefix}/user/register`">Register (name - mobile - password)</nuxt-link>
-        <nuxt-link class="btn-success-outlined" :to="`${prefix}/user/register-username-password`">Register (username password)
+        <nuxt-link class="btn-success-outlined" :to="`${fixturePrefix}/user/login-oauth`">Login (OAuth)</nuxt-link>
+        <nuxt-link class="btn-success" :to="`${fixturePrefix}/user/register`">Register (name - mobile - password)
         </nuxt-link>
-        <nuxt-link class="btn-success-outlined" :to="`${prefix}/user/register-oauth`">Register (OAuth)</nuxt-link>
-        <nuxt-link class="btn-success-outlined" :to="`${prefix}/user/verify`">Verify (SMS)</nuxt-link>
-        <nuxt-link class="btn-success-outlined" :to="`${prefix}/user/verify-otp`">Verify (OTP or Email)</nuxt-link>
-        <nuxt-link class="btn-success-outlined" :to="`${prefix}/user/success`">Success Registration</nuxt-link>
+        <nuxt-link class="btn-success-outlined" :to="`${fixturePrefix}/user/register-username-password`">Register
+          (username password)
+        </nuxt-link>
+        <nuxt-link class="btn-success-outlined" :to="`${fixturePrefix}/user/register-oauth`">Register (OAuth)
+        </nuxt-link>
+        <nuxt-link class="btn-success-outlined" :to="`${fixturePrefix}/user/verify`">Verify (SMS)</nuxt-link>
+        <nuxt-link class="btn-success-outlined" :to="`${fixturePrefix}/user/verify-otp`">Verify (OTP or Email)
+        </nuxt-link>
+        <nuxt-link class="btn-success-outlined" :to="`${fixturePrefix}/user/success`">Success Registration</nuxt-link>
       </div>
-      <div class="gap-3 flex">
-        Archived:
+      <p class="my-2">Archived:</p>
+      <div class="gap-3 flex mt-5">
         <nuxt-link class="btn-success-outlined btn-disabled" :to="`${prefix}/market`">Market Landing</nuxt-link>
         <nuxt-link class="btn-success-outlined btn-disabled" :to="`${prefix}/market/profile`">Profile</nuxt-link>
         <nuxt-link class="btn-success-outlined btn-disabled" :to="`${prefix}/market/profile/orders`">Orders</nuxt-link>
@@ -29,44 +35,9 @@
         <nuxt-link class="btn-success-outlined" :to="`${prefix}/market/products/8364953`">Product page</nuxt-link>
       </div>
     </div>
-    <div class="m-5">
-      <p class="">Next pages:</p>
-      <div class="gap-3 flex">
-        <nuxt-link class="btn-black" target="_blank" href="https://diginext.savyjs.com">Market Landing</nuxt-link>
-        <nuxt-link class="btn-black-outlined" target="_blank" href="https://diginext.savyjs.com">Login</nuxt-link>
-        <nuxt-link class="btn-black" target="_blank" href="https://diginext.savyjs.com/checkout/cart">Cart default
-        </nuxt-link>
-        <nuxt-link class="btn-black" target="_blank" href="https://diginext.savyjs.com/checkout/cart/2">Cart 2
-        </nuxt-link>
-        <nuxt-link class="btn-black" target="_blank" href="https://diginext.savyjs.com/checkout/cart/3">Cart 3
-        </nuxt-link>
-        <nuxt-link class="btn-black" target="_blank" href="https://diginext.savyjs.com/checkout/cart/4">Cart 4
-        </nuxt-link>
-        <nuxt-link class="btn-black" target="_blank" href="https://diginext.savyjs.com/checkout/shipping">Shipping
-        </nuxt-link>
-        <nuxt-link class="btn-black" target="_blank" href="https://diginext.savyjs.com/checkout/payment">Payment
-        </nuxt-link>
-      </div>
-    </div>
-    <div class="m-5">
-      <p class="">Angular pages:</p>
-      <div class="gap-3 flex">
-        <nuxt-link class="btn btn-primary-outlined " target="_blank" href="https://digiangular.savyjs.com/">Market
-          Landing
-        </nuxt-link>
-        <nuxt-link class="btn btn-primary" target="_blank" href="https://digiangular.savyjs.com/products/8364953">
-          Product page
-        </nuxt-link>
-      </div>
-    </div>
-    <div class="m-5">
-      <p class="">Pure HTML pages:</p>
-      <div class="gap-3 flex">
-        <nuxt-link class="btn btn-gray-outlined text-ad" :to="`${prefix}/market/products/8364953`">landing 2</nuxt-link>
-      </div>
-    </div>
   </div>
 </template>
 <script setup>
+const fixturePrefix = "";
 const prefix = "/v1";
 </script>

a24474e69617f6b6413e3143030f7e4794c95970
ehsan <savvyversa@gmail.com>
2023-06-06 15:15:43 +0330

    chore: set CI nitro config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,8 @@ export default defineNuxtConfig({
     css: [
         'assets/font-css/fontiran.css',
         'assets/font-css/style.css'
-    ]
+    ],
+    nitro: {
+        preset: "node"
+    }
 })
